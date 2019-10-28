8b0271ccee1f7304cc5e90b3da1996c4c131b2dd
n8fr8 <nathan@guardianproject.info>
2019-10-28 16:12:32 -0400

    fix bot scripts to improve crypto support

--- bot.js
+++ bot.js
@@ -4,15 +4,15 @@ global.Olm = require('olm');
 // Use path and fs
 const path = require('path');
 const fs = require('fs');
-const util = require('util')
 
-const LocalStorageCryptoStore = require('./node_modules/matrix-js-sdk/lib/crypto/store/localStorage-crypto-store').default;
-
-require("./creds.js");
+var creds = process.argv[3];
+require(creds);
 
 var sdk = require("matrix-js-sdk");
 var clc = require("cli-color");
+var LocalStorage = require('node-localstorage').LocalStorage;
 var rivebot = require ("./modules/rivebot");
+const LocalStorageCryptoStore = require('./node_modules/matrix-js-sdk/lib/crypto/store/localStorage-crypto-store').default;
 
 // Has a directory been given on the command line?
 // Otherwise show information message.
@@ -20,123 +20,98 @@ var targetDir = process.argv[2];
 
 if(targetDir === undefined) {
   console.log('You need to provide a data path');
-  process.exit(1);
+ process.exit(1);
 }
 
 // Create in memory store
 var localpath = path.join(targetDir,'localstorage');
+var localStorage = new LocalStorage(localpath)
 
-// Loading localStorage module
-if (typeof global.localStorage === "undefined" || global.localStorage === null)
-    global.localStorage = new (require('node-localstorage').LocalStorage)(localpath);
+var cryptoStore = new LocalStorageCryptoStore(localStorage);
+sdk.setCryptoStoreFactory(() => cryptoStore);
 
-sdk.setCryptoStoreFactory(() => new LocalStorageCryptoStore(global.localStorage));
+var matrixStore = new sdk.MatrixInMemoryStore({localStorage});
+var sessionStore =  new sdk.WebStorageSessionStore(localStorage);
 
-var matrixStore = new sdk.MatrixInMemoryStore({localStorage: global.localStorage});
-var sessionStore =  new sdk.WebStorageSessionStore(global.localStorage);
-
-var readyToReply = false;
-
-print("creating client for: %s",myBaseUrl);
+// Delay startClient() to give initCrypto() a little room to breathe
+        // This will delay first event delivery, but shouldn't cause any real problems
 var matrixClient = sdk.createClient({
-    baseUrl: myBaseUrl
-});
-
-print("logging in as: %s",myUserId);
-
-matrixClient.login('m.login.password', {user: myUserId, password: myPass, initial_device_display_name: 'Ractive Bot' },  function(err, res) {
-          // Console log
-          console.log('Logged in as ' + res.user_id);
-print("creating full client for token %s and device %s",res.access_token,res.device_id);
-
-  // Create client
-matrixClient = sdk.createClient({
     baseUrl: myBaseUrl,
-    accessToken: res.access_token,
-    userId: myUserId,
     sessionStore: sessionStore,
     store: matrixStore,
-    deviceId: myDeviceId
-  });
+    cryptoStore: cryptoStore,
+    deviceId: myDeviceId,
+    userId: myUserId,
+    accessToken: myAccessToken,
+});
 
-  // Let's start the client.
-	   matrixClient.setDeviceDetails(matrixClient.deviceId, "ractive bot");
-           matrixClient.setDisplayName(myNick);
 
-	   matrixClient.on("event", handleEvent)
-      	   matrixClient.on("Event.decrypted", handleEventDecrypted);
+         matrixClient.initCrypto();
 
-	   print("init matrix crypto");
-	   matrixClient.initCrypto();
-        setTimeout((() => { 
-		print("starting matrix client...");
-  matrixClient.startClient({ initialSyncLimit: 0 });
-	        matrixClient.getRooms();
-		matrixClient.uploadKeys();
-	        readyToReply = true;
-}).bind(this), 3000);
-        }
-);
+      setTimeout((() => { 
+matrixClient.startClient() 
 
+}).bind(this), 500);
 
 // Data structures
+var roomList = [];
 var numMessagesToShow = 0;
 
 // show the room list after syncing.
-matrixClient.on("sync",handleSync);
-
-function handleSync(state, prevState, data) {
+matrixClient.on("sync", function(state, prevState, data) {
     switch (state) {
         case "PREPARED":
-	
+          print("client is prepared!");
+          matrixClient.setDeviceDetails(matrixClient.deviceId,"bot");
+          matrixClient.setDisplayName(myNick);
+          //setRoomList();
         break;
    }
-};
+});
 
 
 function handleEvent (event)
 {
+ print("Got Event: " + event);
  if (event.isEncrypted()) {
             // Will handle it later in Event.decrypted handler
             return;
         }
-//print("HandleEvent: %s",util.inspect(event));
 
  if (event.getType() === "m.room.message")
 	handleIncomingMessage(event);
 }
 
+matrixClient.on("Event", handleEvent)
+matrixClient.on("event", handleEvent)
 
 matrixClient.on("RoomMember.membership", function(event, member) {
-       if (member.membership === "invite" && member.userId === myUserId) {
-           matrixClient.joinRoom(member.roomId).done(function() {
-               print("Auto-joined %s", member.roomId);
-           });
+	printLine(event);
+       if (member && member.membership && member.membership === "invite") {
+	   // if (member.roomId)
+	       print("joining: " + member.roomId);
+           matrixClient.joinRoom(member.roomId).catch((err) => {});
        }
    });
 
 function handleEventDecrypted (event) 
 {
-//print("HandleEventDecrypted: %s",util.inspect(event));
-try {
-   if (event !== 'null' && event !== 'undefined' && event.isDecryptionFailure()) {
-	    print("Decryption failure");
-	    sendErrorMessage(event.getRoomId());
+
+   if (event.isDecryptionFailure()) {
+            //logger.warn("Decryption failure", { event });
+	    print("Decryption failure: " + event);
+            //sendMessage(event.getRoomId(), event.getRoomId(), "I couldn't read what you wrote (decryption failed!)");
             return;
         }
 
  if (event.getType() === "m.room.message")
 	handleIncomingMessage(event);
-  } catch (ex) {
-	print("Decryption error: " + ex);
-	sendErrorMessage(event.getRoomId());
-  }
-	
-
 }
 
+matrixClient.on("Event.decrypted", handleEventDecrypted)
 
 matrixClient.on("Room", function() {
+//    setRoomList();
 });
 
 // print incoming messages.
@@ -145,92 +120,193 @@ matrixClient.on("Room.timeline", function(event, room, toStartOfTimeline) {
         return; // don't print paginated results
     }
 
-    //printLine(event);
-    //handleIncomingMessage(event);
+//    printLine(event);
+    handleIncomingMessage(event);
 });
 
 function handleIncomingMessage (event)
 {
 
-if (event.getSender() !== myUserId && readyToReply)
+if (event.getSender() !== myUserId)
 {
  var body = event.getContent().body;
- print("%s says: %s",event.getSender(),body);
 
- if (typeof body !== 'undefined' && body.length > 0)
+ if (typeof body !== 'undefined' && body !== 'null' && body.length > 0)// && body.startsWith(myNick))
  {
-     if (body.startsWith("@" + myNick))
+
+     if (body.toLowerCase().includes(myNick))
      {
-    	body = body.substring(myNick.length+1);
-	doBotResponse(event.getRoomId(),event.getSender(),body);
-     }
+	     body = body.substring(myNick.length+1);
+	doBotReponse(event.getRoomId(),event.getRoomId(),body);
+	    }
  }
- else
- {
-	sendErrorMessage(event.getRoomId());
 
-
- }
 }
 
 }
 
-
-function sendErrorMessage (roomId)
+function sendMessage (roomId, sender, body)
 {
-matrixClient.sendTextMessage(roomId, "Sorry I couldn't understand that").catch((err) => {
-                                print("err sending message: " + err);
-                        })
-.catch((err) => {
-                    Object.keys(err.devices).forEach((userId) => {
-                        print("rivebot set device known for: %s",userId);
-                        Object.keys(err.devices[userId]).map((deviceId) => {
-    print("rivebot: setting device known for user %s",userId);
-                            matrixClient.setDeviceKnown(userId, deviceId, true).
-                            catch((err) => {
-                                print("err setting device known: " + err);
-                                });;
-                        });
-                    });
-                    // Try again
-                    matrixClient.sendTextMessage(roomId, body).catch((err) => {
-                                print("err sending message: " + err);
-                        });
-                }).finally(function() {});
-
-}
-
-function doBotResponse (roomId, sender, req)
-{
- print("bot is considering a reply...");
- var rep = rivebot.getReply (sender, req);
-    print("bot says: %s to room %s",rep,roomId);
-
             matrixClient.sendTextMessage(roomId, rep)
                 .catch((err) => {
+                        if (typeof Object.keys  !== 'undefined' && typeof err.devices  !== 'undefined' ) {
                     Object.keys(err.devices).forEach((userId) => {
-    			print("device known for: %s",userId);
                         Object.keys(err.devices[userId]).map((deviceId) => {
-    print("rivebot: setting device known for user %s",userId);
-                            matrixClient.setDeviceKnown(userId, deviceId, true).
-			    catch((err) => {
-				print("err setting device known: " + err);
-				});;
+                               print("rivebot: setting device known for user %s",userId);
+                            matrixClient.setDeviceKnown(userId, deviceId, true);
                         });
                     });
+                    }
                     // Try again
-                    matrixClient.sendTextMessage(roomId, rep).catch((err) => {
-				print("err sending message AGAIN: " + err);
-			});
+                    //matrixClient.sendTextMessage(roomId, body);
                 }).finally(function() {});
+}
+
+
+
+function doBotReponse (roomId, sender, req)
+{
+ var rep = rivebot.getReply (sender, req);
+    print("rivebot says: %s",rep);
+            matrixClient.sendTextMessage(roomId, rep)
+                .catch((err) => {
+                        if (typeof Object.keys  !== 'undefined' && typeof err.devices  !== 'undefined' ) {
+                    Object.keys(err.devices).forEach((userId) => {
+                        Object.keys(err.devices[userId]).map((deviceId) => {
+                               print("rivebot: setting device known for user %s",userId);
+                            matrixClient.setDeviceKnown(userId, deviceId, true);
+                        });
+                    });
+                    }
+                    // Try again
+                    //matrixClient.sendTextMessage(roomId, body);
+                }).finally(function() {});
+}
+
+function setRoomList() {
+    roomList = matrixClient.getRooms();
+    roomList.sort(function(a,b) {
+        // < 0 = a comes first (lower index) - we want high indexes = newer
+        var aMsg = a.timeline[a.timeline.length-1];
+        if (!aMsg) {
+            return -1;
+        }
+        var bMsg = b.timeline[b.timeline.length-1];
+        if (!bMsg) {
+            return 1;
+        }
+        if (aMsg.getTs() > bMsg.getTs()) {
+            return 1;
+        }
+        else if (aMsg.getTs() < bMsg.getTs()) {
+            return -1;
+        }
+        return 0;
+    });
+}
+
+function printRoomList() {
+    print("Room List:");
+    var fmts = {
+        "invite": clc.cyanBright,
+        "leave": clc.blackBright
+    };
+    for (var i = 0; i < roomList.length; i++) {
+        var msg = roomList[i].timeline[roomList[i].timeline.length-1];
+        var dateStr = "---";
+        var fmt;
+        if (msg) {
+            dateStr = new Date(msg.getTs()).toISOString().replace(
+                /T/, ' ').replace(/\..+/, '');
+        }
+        var myMembership = roomList[i].getMyMembership();
+        if (myMembership) {
+            fmt = fmts[myMembership];
+        }
+        var roomName = fixWidth(roomList[i].name, 25);
+        print(
+            "[%s] %s (%s members)  %s",
+            i, fmt ? fmt(roomName) : roomName,
+            roomList[i].getJoinedMembers().length,
+            dateStr
+        );
+    }
+}
+
+function printMemberList(room) {
+    var fmts = {
+        "join": clc.green,
+        "ban": clc.red,
+        "invite": clc.blue,
+        "leave": clc.blackBright
+    };
+    var members = room.currentState.getMembers();
+    // sorted based on name.
+    members.sort(function(a, b) {
+        if (a.name > b.name) {
+            return -1;
+        }
+        if (a.name < b.name) {
+            return 1;
+        }
+        return 0;
+    });
+    print("Membership list for room \"%s\"", room.name);
+    print(new Array(room.name.length + 28).join("-"));
+    room.currentState.getMembers().forEach(function(member) {
+        if (!member.membership) {
+            return;
+        }
+        var fmt = fmts[member.membership] || function(a){return a;};
+        var membershipWithPadding = (
+            member.membership + new Array(10 - member.membership.length).join(" ")
+        );
+        print(
+            "%s"+fmt(" :: ")+"%s"+fmt(" (")+"%s"+fmt(")"), 
+            membershipWithPadding, member.name, 
+            (member.userId === myUserId ? "Me" : member.userId),
+            fmt
+        );
+    });
+}
+
+function printRoomInfo(room) {
+    var eventDict = room.currentState.events;
+    var eTypeHeader = "    Event Type(state_key)    ";
+    var sendHeader = "        Sender        ";
+    // pad content to 100
+    var restCount = (
+        100 - "Content".length - " | ".length - " | ".length - 
+        eTypeHeader.length - sendHeader.length
+    );
+    var padSide = new Array(Math.floor(restCount/2)).join(" ");
+    var contentHeader = padSide + "Content" + padSide;
+    print(eTypeHeader+sendHeader+contentHeader);
+    print(new Array(100).join("-"));
+    Object.keys(eventDict).forEach(function(eventType) {
+        if (eventType === "m.room.member") { return; } // use /members instead.
+        Object.keys(eventDict[eventType]).forEach(function(stateKey) {
+            var typeAndKey = eventType + (
+                stateKey.length > 0 ? "("+stateKey+")" : ""
+            );
+            var typeStr = fixWidth(typeAndKey, eTypeHeader.length);
+            var event = eventDict[eventType][stateKey];
+            var sendStr = fixWidth(event.getSender(), sendHeader.length);
+            var contentStr = fixWidth(
+                JSON.stringify(event.getContent()), contentHeader.length
+            );
+            print(typeStr+" | "+sendStr+" | "+contentStr);
+        });
+    })
 }
 
 function printLine(event) {
     var fmt;
     var name = event.sender ? event.sender.name : event.getSender();
-    var time = new Date(
-        event.getTs()
-    ).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+    //ar time = new Date(
+    //    event.getTs()
+    //).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+	var time = event.getTs();
     var separator = "<<<";
     if (event.getSender() === myUserId) {
         name = "Me";
